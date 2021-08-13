e73c3906ad448f00950849a2b503cee200dd2cb1
neyderF <49882090+neyderF@users.noreply.github.com>
2021-08-12 21:47:46 -0500

    sequelize module

---
+++ src/models/empleado.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class empleado extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  empleado.init(
+    {
+      id_empleado: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      id_fundacion: DataTypes.INTEGER,
+      correo: DataTypes.STRING(70),
+      contrasenia: DataTypes.STRING(300),
+      nombre: DataTypes.STRING(70),
+      rol: DataTypes.STRING(45),
+    },
+    {
+      sequelize,
+      modelName: "empleado",
+    }
+  );
+  return empleado;
+};
